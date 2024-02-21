3a7699da7befa22679cde35850f6553e1b585a17
valberjr <valber@duck.com>
2024-02-21 14:11:30 -0300

    material table css

--- angular-ui/src/app/courses/courses/courses.component.ts
+++ angular-ui/src/app/courses/courses/courses.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { Course } from '../models/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule, MatToolbarModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss',
 })
