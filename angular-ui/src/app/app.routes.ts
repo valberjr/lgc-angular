e900cb0ac44c95e760a9ba62bcfb10630d88abbf
valberjr <valber@duck.com>
2024-02-21 12:11:14 -0300

    add lazy loading route for courses component

--- angular-ui/src/app/app.routes.ts
+++ angular-ui/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'courses',
+  },
+  {
+    path: 'courses',
+    loadComponent: () =>
+      import('./courses/courses.component').then((m) => m.CoursesComponent),
+  },
+];
