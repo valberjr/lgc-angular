f64cbb23cc39af51e95519503683dc8665ef14fa
valberjr <valber@duck.com>
2024-02-23 16:38:10 -0300

    fix cors adding proxy configuration

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, first, tap } from 'rxjs';
+import { first, tap } from 'rxjs';
 import { Course } from '../models/course';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/courses.json';
+  private readonly API = '/api/courses';
 
   constructor(private httpClient: HttpClient) {}
 
