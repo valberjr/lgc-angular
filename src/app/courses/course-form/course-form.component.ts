232bca68d7cf58adf8326b095161b7e72c40d6b2
valberjr <valber@duck.com>
2024-02-29 16:16:45 -0300

    implement error handling for HTTP POST request when saving data

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -4,7 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { error } from 'console';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -16,20 +20,37 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatCardModule,
     MatToolbarModule,
     MatButtonModule,
+    MatSelectModule,
+    MatSnackBarModule,
   ],
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss',
 })
 export class CourseFormComponent {
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
     });
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (result) => console.log(result),
+      (error) => this.onError()
+    );
+  }
 
   onCancel() {}
+
+  private onError() {
+    this._snackBar.open('Error on saving course', '', {
+      duration: 5000,
+    });
+  }
 }
