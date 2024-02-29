794b186e895ab5359a5681b1910cf0efb8bd75a6
valberjr <valber@duck.com>
2024-02-29 12:05:37 -0300

    add course creation form

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   templateUrl: './course-form.component.html',
-  styleUrl: './course-form.component.scss'
+  styleUrl: './course-form.component.scss',
 })
 export class CourseFormComponent {
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null],
+    });
+  }
 
+  onSubmit() {}
+
+  onCancel() {}
 }
